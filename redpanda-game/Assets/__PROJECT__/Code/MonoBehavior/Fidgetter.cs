bc2d20ebe8f994b0b0d3d71bfb13e5fac11572e8
MeekoSoup <weston.campbell1@gmail.com>
2019-03-01 14:27:56 -0800

    Updated MainMenu & Added OdinInspector asset
    - Put Unity-Chan in the main menu scene and attached fidgetting animation and a script to her.
    - Imported Odin Inspector
    - Imported Huge Mocap Library part 2

---
+++ redpanda-game/Assets/__PROJECT__/Code/MonoBehavior/Fidgetter.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fidgetter : MonoBehaviour
+{
+    public string[] animations;
+    public float fidgetTime = 5f;
+
+    private float fidgetTimeElapsed;
+    private Animator anim;
+
+    private void Awake()
+    {
+        fidgetTimeElapsed = fidgetTime;
+        anim = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        if (fidgetTimeElapsed <= 0)
+        {
+            fidgetTimeElapsed = fidgetTime;
+            int r = Random.Range(0, animations.Length);
+            anim.Play(animations[r], 0, 0);
+        }
+        fidgetTimeElapsed -= Time.deltaTime;
+    }
+}
