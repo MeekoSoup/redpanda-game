0ed628dae7ea152b01ea6961fbc0bc45a634dcc3
MeekoSoup <weston.campbell1@gmail.com>
2019-03-12 19:13:00 -0700

    Screwing around with Spinners detecting distance

---
+++ redpanda-game/Assets/__PROJECT__/Code/MonoBehavior/Spinner.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+public class Spinner : MonoBehaviour
+{
+    public float spinSpeed = 20f;
+    public float spinDist = 50f;
+
+    private SceneManager sm;
+
+    [ShowInInspector]
+    private float dist;
+    [ShowInInspector]
+    private float math;
+
+    private void Awake()
+    {
+        sm = GameObject.FindGameObjectWithTag("SceneManager").GetComponent<SceneManager>();
+    }
+
+    private void Update()
+    {
+        Vector3 offset = transform.position - sm.player.transform.position;
+        dist = offset.sqrMagnitude;
+        dist = Vector3.Distance(transform.position, sm.player.transform.position);
+        math = spinSpeed / (dist + 0.1f);
+        if (dist < spinDist)
+        {
+            transform.Rotate(Vector3.forward * math * Time.deltaTime);
+        }
+        else
+        {
+            transform.Rotate(Vector3.forward * spinSpeed * Time.deltaTime);
+        }
+    }
+}
