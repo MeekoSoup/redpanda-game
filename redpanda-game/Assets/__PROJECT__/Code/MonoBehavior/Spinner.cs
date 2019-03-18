e8951442125d56cc546da2b0a4ecc9943c32a265
MeekoSoup <weston.campbell1@gmail.com>
2019-03-17 18:24:59 -0700

    Formalized player referencing, and got Spinner working
    Also added script execution order for Spinner, PlayerSpawner, and GameManager

--- redpanda-game/Assets/__PROJECT__/Code/MonoBehavior/Spinner.cs
+++ redpanda-game/Assets/__PROJECT__/Code/MonoBehavior/Spinner.cs
@@ -6,15 +6,38 @@ using UnityEngine;
 public class Spinner : MonoBehaviour
 {
     public float spinSpeed = 20f;
-    public float spinDist = 50f;
+    public float spinSpeedMin = 1f;
+    public float spinSpeedMax = 25f;
+
+    // Cached private properties
+    private GameObject player = null;
+    private float dist = 0;
+    private float spinFactor = 0;
 
     private void Awake()
     {
+        if (GameManager.instance == null)
+        {
+            Debug.Log("GameManager.instance == null");
+            return;
+        }
 
+        if (GameManager.instance.GetPlayer() == null)
+        {
+            Debug.Log("GameManager.instance.GetPlayer() == null");
+            return;
+        }
+
+        player = GameManager.instance.GetPlayer();
     }
 
     private void Update()
     {
-        
+        if (player == null)
+            return;
+
+        dist = (transform.position - player.transform.position).sqrMagnitude;
+        spinFactor = Mathf.Clamp(spinSpeed / (dist + 1) * spinSpeed * Time.deltaTime, spinSpeedMin, spinSpeedMax);
+        transform.Rotate(Vector3.forward * spinFactor);
     }
 }
