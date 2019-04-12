4619bea9de0b8180e41ae0ca23a3391f20d24ae9
MeekoSoup <weston.campbell1@gmail.com>
2019-04-11 22:19:43 -0700

    Still messing around with the character controllers...

---
+++ redpanda-game/Assets/__PROJECT__/Code/MonoBehavior/MouseMoveInput.cs
@@ -0,0 +1,34 @@
+﻿public class MouseMoveInput : com.ootii.Input.UnityInputSource
+{
+    /*
+    public override bool IsViewingActivated
+    {
+        get
+        {
+            if (!_IsEnabled) { return false; }
+            
+            bool lValue = false;
+
+            if (_IsXboxControllerEnabled)
+            {
+#if UNITY_EDITOR_OSX || UNITY_STANDALONE_OSX
+                    lValue = (UnityEngine.Input.GetAxis("MXRightStickX") != 0f);
+                    if (!lValue) { lValue = (UnityEngine.Input.GetAxis("MXRightStickY") != 0f); }
+#else
+                lValue = (UnityEngine.Input.GetAxis("WXRightStickX") != 0f);
+                if (!lValue) { lValue = (UnityEngine.Input.GetAxis("WXRightStickY") != 0f); }
+#endif
+            }
+
+            if (!lValue)
+            {
+                return true;
+            }
+
+            return lValue;
+            
+            return true;
+        }
+    }
+    */
+}
