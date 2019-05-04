e2c48e1beb19eece1f0f5640654adbade163ee9e
MeekoSoup <weston.campbell1@gmail.com>
2019-05-04 08:29:38 -0700

    Added collectables summary
    -Gives count of coins versus red pandas
    -Offers worth for each type and quality
    -Type and quality added to collectables
    -Built out level01 a bit

---
+++ redpanda-game/Assets/__PROJECT__/Code/Editor/CollectablesEditor.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(Collectables))]
+public class CollectablesEditor : Editor
+{
+    [SerializeField]
+    private int coins, 
+                copper, 
+                silver, 
+                gold, 
+                copperWorth, 
+                silverWorth, 
+                goldWorth, 
+                redpanda, 
+                redpandaWorth;
+
+    [SerializeField]
+    private Collectable[] collectables;
+
+    public override void OnInspectorGUI()
+    {
+        coins = 0;
+        copper = 0;
+        silver = 0;
+        gold = 0;
+        copperWorth = 0;
+        silverWorth = 0;
+        goldWorth = 0;
+        redpanda = 0;
+        redpandaWorth = 0;
+
+        base.OnInspectorGUI();
+
+        var collectablesScript = target as Collectables;
+        collectables = collectablesScript.gameObject.GetComponentsInChildren<Collectable>();
+
+        foreach (Collectable collectable in collectables)
+        {
+            if (collectable.collectableType == Collectable.CollectableType.Coin)
+                coins++;
+            if (collectable.collectableType == Collectable.CollectableType.RedPanda)
+                redpanda++;
+            if (collectable.quality == Collectable.Quality.Copper)
+            {
+                copper++;
+                copperWorth += Collectable.COPPER_WORTH;
+            }
+            if (collectable.quality == Collectable.Quality.Silver)
+            {
+                silver++;
+                silverWorth += Collectable.SILVER_WORTH;
+            }
+            if (collectable.quality == Collectable.Quality.Gold)
+            {
+                gold++;
+                goldWorth += Collectable.GOLD_WORTH;
+            }
+            if (collectable.collectableType == Collectable.CollectableType.RedPanda)
+            {
+                redpanda++;
+                redpandaWorth += Collectable.REDPANDA_WORTH;
+            }
+        }
+
+        int coinsWorth = copperWorth + silverWorth + goldWorth;
+        int totalWorth = coinsWorth + redpandaWorth;
+
+        GUILayout.Label(string.Format("Coins: {0} ({1}) {2:0.0}%", coins, coinsWorth, ((float)coinsWorth / totalWorth) * 100));
+        GUILayout.Label(string.Format("Copper: {0} ({1}) {2:0.0}%", copper, copperWorth, ((float)copperWorth / totalWorth) * 100));
+        GUILayout.Label(string.Format("Silver: {0} ({1}) {2:0.0}%", silver, silverWorth, ((float)silverWorth / totalWorth) * 100));
+        GUILayout.Label(string.Format("Gold: {0} ({1}) {2:0.0}%", gold, goldWorth, ((float)goldWorth / totalWorth) * 100));
+        GUILayout.Label(string.Format("RedPanda: {0} ({1}) {2:0.0}%", redpanda, redpandaWorth, ((float)redpandaWorth / totalWorth) * 100));
+    }
+}
---
+++ redpanda-game/Assets/__PROJECT__/Code/MonoBehavior/Collectable.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectable : BaseBehavior
+{
+    public enum CollectableType { Coin, RedPanda }
+    public enum Quality { Copper, Silver, Gold }
+
+    public const int COPPER_WORTH = 5,
+                     SILVER_WORTH = 25,
+                     GOLD_WORTH = 150,
+                     REDPANDA_WORTH = 1000;
+
+    public CollectableType collectableType = CollectableType.Coin;
+    public Quality quality = Quality.Copper;
+    [Space]
+    public int worth = 5;
+    [Space]
+    public float seekDist = 5f;
+    public float collectDist = 0.5f;
+    public float speed = 1f;
+    [Space]
+    public GameObject coinShatter;
+
+    private bool seeking = false;
+    private GameObject player;
+
+    private void Awake()
+    {
+        player = GameManager.instance.Player;
+    }
+
+    private void Update()
+    {
+        if (player == null)
+        {
+            Debug.LogError("Error: Player not found.");
+            return;
+        }
+
+        float dist = (player.transform.position - transform.position).sqrMagnitude;
+
+        if (seeking)
+        {
+            transform.position = Vector3.Lerp(transform.position, player.transform.position, speed * time.deltaTime);
+            if (dist <= collectDist)
+                Collect();
+
+            return;
+        }
+
+        if (dist <= seekDist)
+            seeking = true;
+    }
+
+    public void Collect()
+    {
+        Instantiate(coinShatter, transform.position, transform.rotation);
+        GameManager.instance.score += worth;
+        //Destroy(gameObject);
+        gameObject.SetActive(false);
+    }
+}
